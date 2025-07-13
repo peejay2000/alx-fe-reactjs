c14f301c63dd6c8adeb10804dfebec47c8c734dd
James Precious Uthene <preshybee000@gmail.com>
2025-07-13 21:06:13 +0100

    task

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile'
 
 
 
@@ -39,6 +40,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+    <div>
+      <h1>User Profile</h1>
+      <UserProfile name="Alice" age={25} bio="Loves hiking and photography." />
+    </div>
     
     </>
   )
