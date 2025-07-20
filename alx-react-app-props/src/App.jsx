c9d837e31ebe2ad8386edc039a4954f291152969
James Precious Uthene <preshybee000@gmail.com>
2025-07-20 11:22:15 +0100

    task

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import ProfilePage from '../src/components/ProfilePage';
+import UserContext from '../src/components/UserProfile';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Precious Clement Thene", email: "preshybee000@gmail.com" };
 
   return (
     <UserContext.Provider value={userData}>
