940df9debea0799a86ae2fe1250938735c51f344
James Precious Uthene <preshybee000@gmail.com>
2025-08-02 13:08:49 +0100

    task

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,25 @@
-import api from './api';
+import axios from 'axios';
 
-export const fetchUserData = async (username) => {
-  const response = await api.get(`/users/${username}`);
+// Use GitHub Search API for advanced user search
+export const searchUsers = async ({ username, location, minRepos, page = 1 }) => {
+  let query = [];
+
+  if (username) query.push(`${username} in:login`);
+  if (location) query.push(`location:${location}`);
+  if (minRepos) query.push(`repos:>=${minRepos}`);
+
+  const q = query.join('+');
+
+  const response = await axios.get(`https://api.github.com/search/users`, {
+    params: {
+      q,
+      per_page: 10,
+      page,
+    },
+    headers: {
+      Authorization: `token ${import.meta.env.VITE_GITHUB_API_TOKEN || ''}`,
+    },
+  });
+
   return response.data;
 };
