760ee92dc0d9fad78dc5baf1e7fb717b908952a5
James Precious Uthene <preshybee000@gmail.com>
2025-08-23 14:37:24 +0100

    Router

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Blog from "./pages/Blog";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div>
+      <nav className="flex gap-4 p-4 bg-gray-200">
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/blog/1">Blog</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
