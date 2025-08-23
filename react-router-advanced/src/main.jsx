94fe3f0a9ac1ddcff08164d9db8095d74173a6e8
James Precious Uthene <preshybee000@gmail.com>
2025-08-23 14:58:40 +0100

    Router

--- react-router-advanced/src/main.jsx
+++ react-router-advanced/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";  // ✅ BrowserRouter lives here
+import { BrowserRouter } from "react-router-dom"; // ✅ put BrowserRouter here
 import App from "./App";
 import "./index.css";
 
