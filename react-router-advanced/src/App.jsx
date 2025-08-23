304f7bcd13afdefc965ece8f5be95f3f781601e2
James Precious Uthene <preshybee000@gmail.com>
2025-08-23 15:01:22 +0100

    Router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,34 +1,29 @@
-import { Routes, Route, Link } from "react-router-dom";
+// src/App.jsx
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Blog from "./pages/Blog";
 import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <div>
-      <nav className="flex gap-4 p-4 bg-gray-200">
-        <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/blog/1">Blog</Link>
-        <Link to="/login">Login</Link>
-      </nav>
+    <BrowserRouter>
+      <div>
+        <nav className="flex gap-4 p-4 bg-gray-200">
+          <Link to="/">Home</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/blog/1">Blog</Link>
+          <Link to="/login">Login</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/*" element={<Profile />} />
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
